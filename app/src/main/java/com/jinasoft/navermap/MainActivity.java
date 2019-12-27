8accdf13bae07ad35fe42b175df4e8c490a38e85
LEE <ffgoo@naver.com>
2019-12-27 17:32:09 +0900

    위도 경도 마커 JSONArray String형 을 Double형으로 바꾸는 방법찾기

--- app/src/main/java/com/jinasoft/navermap/MainActivity.java
+++ app/src/main/java/com/jinasoft/navermap/MainActivity.java
@@ -1,26 +1,225 @@
 package com.jinasoft.navermap;
 
 
+
+
+
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
 
-import com.naver.maps.map.NaverMapSdk;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
 
-        @Override
+import com.google.gson.JsonParser;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private Fragment NMAP;
+
+    private ArrayList<String> name;
+    private ArrayList<String> phone_number;
+    private ArrayList<String> address;
+    private ArrayList<String> longitude;
+    private ArrayList<String> latitude;
+    private JSONArray jsonArray;
+    private TextView textView;
+
+
+    @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
 
-            NaverMapSdk.getInstance(this).setClient(
-                    new NaverMapSdk.NaverCloudPlatformClient("d4onuthfhd"));
+            textView =findViewById(R.id.textView) ;
+            name = new ArrayList<>();
+            phone_number = new ArrayList<>();
+            address = new ArrayList<>();
+            longitude = new ArrayList<>();
+            latitude = new ArrayList<>();
+
+
+
+            MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.NMAP);
+            if (mapFragment == null) {
+                mapFragment = MapFragment.newInstance();
+                getSupportFragmentManager().beginTransaction().add(R.id.NMAP, mapFragment).commit();
+            }
+
+            mapFragment.getMapAsync((OnMapReadyCallback) this);
+        }
+
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+
+        InsertData task = new InsertData();
+        task.execute("http://58.230.203.182/Landpage/Realtor_List.php");
+
+  //      double LAT = Double.valueOf(latitude);
+//        double LNG = Double.parseDouble(String.valueOf(longitude));
+
+//        Double ls =126.9567534;
+
+        Double ls =126.9567534;
+        Double ly =37.3942906;
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(ly, ls));
+        marker.setMap(naverMap);
+
+    }
+    class InsertData extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
 
 
         }
 
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+
+            try {
+
+
+                JSONObject Land = new JSONObject(result);
+
+
+                        JSONArray jsonArray = Land.getJSONArray("Response");
+
+                for(int i = 0 ; i<jsonArray.length(); i++){
+                    JSONObject subJsonObject = jsonArray.getJSONObject(i);
+
+                String sNAME = subJsonObject.getString("name");
+                String sPHONE = subJsonObject.getString("cell_phone_number");
+                String sADD = subJsonObject.getString("address");
+                String sLAT = subJsonObject.getString("latitude");
+                String sLNG = subJsonObject.getString("longitude");
+                name.add(sNAME);
+                phone_number.add(sPHONE);
+                address.add(sADD);
+                latitude.add(sLAT);
+                longitude.add(sLNG);
+
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            textView.setText(latitude.toString()+"\n" + longitude.toString() );
+
+
+
+
+
+       //    textView.setText(result);
+     }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+
+            String postParameters ="";
+
+
+            try {
+
+                URL url = new URL("http://58.230.203.182/Landpage/Realtor_List.php");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
     }
+
+
+}
+
+
+
 
 
